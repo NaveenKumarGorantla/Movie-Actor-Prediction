413aab258d46e7313125bbd2951afb06288f9f12
NaveenKumarGorantla <naveenkumargorantla9@gmail.com>
2021-04-26 23:16:16 +0000

    updated setup files

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 #setup.py file
+
+from setuptools import setup, find_packages
+setup(
+	name='unredactor',
+	version='1.0',
+	author='Naveen Kumar Gorantla',
+	authour_email='Naveen.Kumar.Gorantla-1@ou.edu',
+	packages=find_packages(exclude=('tests', 'docs')),
+	setup_requires=['pytest-runner'],
+	tests_require=['pytest']
+)
+
