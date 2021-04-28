6ea2ff9ac6d4b2ff5d3c92cf59c762caa1da532c
NaveenKumarGorantla <naveenkumargorantla9@gmail.com>
2021-04-28 03:22:27 +0000

    Latest Changes

--- redactor/unredactor.py
+++ redactor/unredactor.py
@@ -1,4 +1,7 @@
 #python file
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import numpy as np
 from nltk.tokenize import word_tokenize,sent_tokenize
 import nltk
 import re
@@ -18,7 +21,7 @@ def get_traindata(input_files):
             list_filepaths.append(filepaths)
 
     filepaths_list = nltk.flatten(list_filepaths)
-    filepaths_list = filepaths_list[:100]
+    filepaths_list = filepaths_list[:200]
     print('Text files in Train data',len(filepaths_list))
     #print(filepaths_list)
     for filepath in filepaths_list:
@@ -43,8 +46,8 @@ def get_testdata(input_files):
             list_filepaths.append(filepaths)
 
     filepaths_list = nltk.flatten(list_filepaths)
-    filepaths_list = filepaths_list[0:50]
-    print('Text files in Text data',len(filepaths_list))
+    filepaths_list = filepaths_list[0:1]
+    print('Text files in Test data',len(filepaths_list))
     #print(filepaths_list)
     for filepath in filepaths_list:
         fileopen = open(filepath)
@@ -74,52 +77,47 @@ def get_named_entities(list_filedata):
             if ( entity.label() == 'PERSON'):
                 for leaf in entity.leaves():
                     word_name.append(leaf[0])
-                    print(leaf[0])
+                    #print(leaf[0])
                     total_words += 1
                     length_words[i]= len( leaf[0])
                     i = i + 1
                 word_name = ' '.join(word_name)
-                print(word_name)
+                #print(word_name)
                 total_length = len(word_name)
-                dict = {'name_feature': word_name,'Count feature of words': total_words, 'Feature length 1': length_words[0], 'Feature Length 2': length_words[1], 'Feature Length 3': length_words[2], 'total_feature_length': total_length}
+                dict = {'F1': word_name,'F2': total_words, 'F3': length_words[0], 'F4': length_words[1], 'F5': length_words[2], 'F6': total_length}
                 Dictionary_list.append(dict)
-    print(Dictionary_list[0:10])
+    #print(Dictionary_list[0:10])
     return Dictionary_list
 
 
-#Training
+def Model_training():
 
-inputfiledata = get_traindata([['aclImdb/train/pos/*.txt']])
-#print("traindata",inputfiledata[0:10])
-trainingdata=get_named_entities(inputfiledata)
-#print('training data',trainingdata)
+    train_data = get_traindata([['aclImdb/train/pos/*.txt']])
+    if ( len(train_data) > 0):
 
-from sklearn.feature_extraction import DictVectorizer
-import numpy as np
+        named_entities=get_named_entities(train_data)
+        y_train = []
+        x_train = []
+        for item in named_entities:
+            y_train.append(item['F1'])
+            del item['F1']
+            x_train.append(item)
 
-#2 training the model
+        vec = DictVectorizer(sparse=False)
+        x_train = vec.fit_transform(x_train)
+        #print(x_train)
+        y_train = np.array(y_train)
+        #print("Features:", vec.get_feature_names())
+        #Training the model
+        model= MultinomialNB()
+        model.fit(x_train, y_train)
 
-y_train = []
-x_train = []
-for item in trainingdata:
-    y_train.append(item['name_feature'])
-    del item['name_feature']
-    x_train.append(item)
+    else:
+        return None
 
-vec = DictVectorizer(sparse=False)
-x_train = vec.fit_transform(x_train)
-#print(x_train)
-y_train = np.array(y_train)
-#print("Features:", vec.get_feature_names())
+    return model
 
-from sklearn.naive_bayes import MultinomialNB
-#Training the model
-model= MultinomialNB()
-model.fit(x_train, y_train)
-
-
-
-#redaction
+model = Model_training()
 
 def get_redactednameentities(totaldata):
     totalredacteddata=[]
@@ -133,13 +131,16 @@ def get_redactednameentities(totaldata):
             #print(chunk)
             for leave in chunk.leaves():
                 name.append(leave[0])
+                print(leave[0])
             personslist.append(' '.join(name))
+        print('test data person names',personslist)    
         setofnameslist = list(set(personslist))
         #sorting based on length of the name and sorting in descending order
         setofnameslist=sorted(setofnameslist,key=lambda name: len(name),reverse=True)
-        #print(setofnameslist)
+        print('set of name list :',setofnameslist)
         for itemname in setofnameslist:
             itemnamelist=itemname.split()
+            print(itemnamelist)
            # print(itemname,len(itemname))
             #doc = doc.replace(itemname,str( '█' * len(itemname)))
             if(len(itemnamelist)==1):
@@ -176,7 +177,7 @@ def ExtractFeatures_redact_data(totaldata):
             if(len(wordlengthlist)<4):
                 wordlengthlist.append(0);wordlengthlist.append(0);wordlengthlist.append(0)
             if(count>0):
-                redacted_name_features = {'name': ' '.join(wordlist), 'wordscount': count, 'Firstwordlength': wordlengthlist[0],'Secondwordlength': wordlengthlist[1], 'Thirdwordlength': wordlengthlist[2],'name_length': len(' '.join(wordlist))}
+                redacted_name_features = {'F1': ' '.join(wordlist), 'F2': count, 'F3': wordlengthlist[0],'F4': wordlengthlist[1], 'F5': wordlengthlist[2],'F6': len(' '.join(wordlist))}
                 #print(redacted_name_features)
                 redactednames_featureslist.append(redacted_name_features)
     return redactednames_featureslist
@@ -188,12 +189,14 @@ def ExtractFeatures_redact_data(totaldata):
 #Testing
 
 testfiledata=get_testdata([['aclImdb/test/pos/*.txt']])
+print(testfiledata)
 redacteddocuments=get_redactednameentities(testfiledata)
-#print(redacteddocuments)
+print(redacteddocuments)
 redactedfeatureslist=ExtractFeatures_redact_data(redacteddocuments)
-#print(redactedfeatureslist)
+print(redactedfeatureslist)
 
 #predicting
+vec = DictVectorizer(sparse=False)
 x_test=[]
 y_test=[]
 redacteddocumentcount=0
@@ -201,11 +204,15 @@ for item in redactedfeatureslist:
     redacteddocumentcount+=1
     #print(redacteddocumentcount)
     # print(name_dict['name'])
-    y_test.append(item['name'])
-    del item['name']
+    y_test.append(item['F1'])
+    del item['F1']
     x_test.append(item)
 x_test = vec.fit_transform(x_test)
+
+#model = Model_training()
+
 y_pred =  model.predict(x_test)
+#print('Score', model.score(x_test, y_test))
 #print(y_pred)
 newfilepath = os.path.join(os.getcwd(),'outputfile')
 if not os.path.exists(newfilepath):
@@ -215,7 +222,9 @@ if not os.path.exists(newfilepath):
             outputfile.write(item)
 elif os.path.exists(newfilepath):
     with open(os.path.join(newfilepath, 'output'), 'w') as outputfile:
+        count = 1
         for item in y_pred: 
-            outputfile.write(" " + item+ " ")
+            outputfile.write(str(count)+"." +" "+ item+ " / ")
+            count = count + 1
 
 
